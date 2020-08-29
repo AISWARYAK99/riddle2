a538a143065cf5c8fba325bba732c6db0e1e74f1
AISWARYAK99 <66054986+AISWARYAK99@users.noreply.github.com>
2020-08-29 19:22:40 +0530

    linked riddle 3 to 2

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 var pass;
 pass = prompt("please enter the answer to proceed");
 if (pass == "NOON") {
-    document.location.href = "";
+    document.location.href = "https://github.com/AISWARYAK99/riddle3";
 } else {
     document.location.href="error1.html";
 }  
